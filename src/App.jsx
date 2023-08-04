569b363d18a1f3c1cfba238cb8af86594dfde3dd
Parshva Dani <parshva.dani.btech2021@sitpune.edu.in>
2023-08-04 16:10:06 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import  CssBaseline  from '@mui/material/CssBaseline'
+import TodoList from './TodoList'
+import Navbar from './Navbar'
+function App() {
+ 
+
+  return (
+    <>
+      <CssBaseline />
+      <Navbar />
+      <TodoList />
+    </>
+  )
+}
+
+export default App
